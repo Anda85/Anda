376817403e18593a572521a4468da7988177ee24
Anda85 <91291912+Anda85@users.noreply.github.com>
2022-10-26 15:31:27 +0200

    Add files via upload

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,20 @@
+function randomNo() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+var d1 = randomNo();
+var d2 = randomNo();
+
+const loc = "images/dice";
+document.querySelector(".img1").setAttribute("src", loc + d1 + ".png");
+document.querySelector(".img2").setAttribute("src", loc + d2 + ".png");
+
+if (d1 > d2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins";
+}
+else if (d1 < d2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
